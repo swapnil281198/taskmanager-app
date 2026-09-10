47b71af6d00cb8b11bdeeb4fc27fae2dbcaf104e
swapnil281198 <swapnil281198@gmail.com>
2026-09-10 11:17:15 +0530

    Initialize React application in index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from "react"*
+import ReactDOM from "react-dom/c*ient";
+import App from "./App";
+
+c*nst root = ReactDOM.createRoot(
+ d*cument.getElementById("root")
+);
+
+*oot.render(
+ <React.StrictMode>
+  * <App />
+ </React.StrictMode>
+);
+`*
